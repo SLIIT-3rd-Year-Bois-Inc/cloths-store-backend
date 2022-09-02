31162b8da98aa8fc4af16058a75042393d8a09e1
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-09-02 23:28:09 +0530

    completed add,update, archive, getOneItem, findItems

--- controllers/stock.js
+++ controllers/stock.js
@@ -1,17 +1,131 @@
-import express, { Request, Response } from "express";
+import express, { Request, response, Response } from "express";
+let Product = require("../models/Product");
 
 /**
  * @param {express.Request} req
  * @param {express.Response} res
  */
-function sayHello(req, res) {
-  res.send("Hello");
+function getProductsForCustomer(req, res) {
+  Product.find()
+    .select("_id")
+    .then((product) => {
+      res.json(product);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+function getProduct(req, res) {
+  let id = req.query.id;
+
+  Product.findById(id)
+    .then((product) => {
+      res.json(product);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function addNewProduct(req, res) {
+  console.log(req.body);
+  const name = req.body.name;
+  const price = Number(req.body.price);
+  const gender = req.body.gender;
+  const tags = req.body.tags;
+  const imagesUrls = req.body.imagesUrls;
+  const description = req.body.description;
+  const color = req.body.color;
+  const quantity = req.body.quantity;
+
+  const newProduct = new Product({
+    name,
+    price,
+    gender,
+    tags,
+    imagesUrls,
+    description,
+    color,
+    quantity,
+  });
+
+  console.log(newProduct);
+
+  newProduct
+    .save()
+    .then(() => {
+      res.json("product added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+function archiveProduct(req, res) {}
+async function updateProduct(req, res) {
+  const {
+    _id,
+    name,
+    price,
+    gender,
+    tags,
+    imagesUrls,
+    description,
+    color,
+    quantity,
+  } = req.body;
+
+  const updateProduct = {
+    name,
+    price,
+    gender,
+    tags,
+    imagesUrls,
+    description,
+    color,
+    quantity,
+  };
+
+  const update = await Product.findByIdAndUpdate(_id, updateProduct)
+    .then((Response) => {
+      res.status(200).send({ status: "product updated", updatedobj: Response });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+}
+async function archiveProduct(req, res) {
+  const { _id, archived } = req.body;
+  console.log(req.body);
+  const updateProduct = {
+    archived,
+  };
+  const update = await Product.findByIdAndUpdate(_id, updateProduct)
+    .then((Response) => {
+      res.status(200).send({ status: "product updated", updatedobj: Response });
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+}
+
+function deleteProduct(req, res) {}
 
 export function stockRouter() {
   const router = express.Router();
 
-  router.get("/hello", sayHello);
+  router.get("/getCusProducts", getProductsForCustomer);
+  router.get("/getProduct", getProduct);
+  router.post("/newProduct", addNewProduct);
+  router.delete("/deleteProduct", deleteProduct);
+  router.put("/archiveProduct", archiveProduct);
+  router.put("/updateProduct", updateProduct);
 
   return router;
 }
---
+++ models/Product.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    uppercase: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    index: true,
+    required: true,
+  },
+  gender: {
+    type: String,
+    maxlength: 1,
+    required: true,
+  },
+  tags: [
+    {
+      type: String,
+    },
+  ],
+  imagesUrls: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  quantity: {
+    type: Map,
+    of: Number,
+  },
+  archived: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
